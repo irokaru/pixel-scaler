7430971f72060024b9261717ba95f4f1a5e37771
irokaru <karuta@nononotyaya.net>
2020-10-22 23:21:48 +0900

    add footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="app">
-    <h1>ドット絵のサイズをいい感じにリサイズするやつ</h1>
+    <main>
+      <h1>ドット絵のサイズをいい感じにリサイズするやつ</h1>
 
-    <a class="box circle hover active" href="https://twitter.com/share/?text=ドット絵のサイズをいい感じにリサイズするやつ" target="_blank">
-      <i class="fab fa-twitter"></i>test
-    </a>
+      <a class="box circle hover active" href="https://twitter.com/share/?text=ドット絵のサイズをいい感じにリサイズするやつ" target="_blank">
+        <i class="fab fa-twitter"></i>test
+      </a>
+    </main>
+
+    <footer>
+      (C) 2020 ののの茶屋.
+    </footer>
   </div>
 </template>
 
