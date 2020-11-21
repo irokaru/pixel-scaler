b6ceb0f45eddf35a700f4c7d2daaf7b6e91cd0fc
irokaru <karuta@nononotyaya.net>
2020-11-21 19:22:24 +0900

    refactor

--- src/components/LinkContainer.vue
+++ src/components/LinkContainer.vue
@@ -1,19 +1,39 @@
 <template>
   <div>
-    <a href="https://bit.ly/2IOgFa3" target="_blank" class="box circle margin-lr-1 hover active">
-      <v-fa :icon="['fas', 'share-alt-square']"/> <v-fa :icon="['fab', 'twitter']"/> 共有
-    </a>
-
-    <a href="https://twitter.com/irokaru" target="_blank" class="box circle margin-lr-1 hover active">
-      <v-fa :icon="['fab', 'twitter']"/> 作った人
-    </a>
-
-    <a href="https://github.com/irokaru/pixel-scaler" target="_blank" class="box circle margin-lr-1 hover active">
-      <v-fa :icon="['fab', 'github']"/> リポジトリ
-    </a>
-
-    <a href="https://nononotyaya.booth.pm/items/2517679" target="_blank" class="box circle margin-lr-1 hover active">
-      <v-fa icon="images"/> Booth
+    <a :href="link.url" target="_blank" class="box circle margin-lr-1 hover active"
+       v-for="link in links" :key="link.url">
+      <v-fa :icon="link.icon"/> {{link.text}}
     </a>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        {
+          url: 'https://bit.ly/2IOgFa3',
+          icon: ['fas', 'share-alt-square'],
+          text: '共有',
+        },
+        {
+          url: 'https://twitter.com/irokaru',
+          icon: ['fab', 'twitter'],
+          text: '作った人',
+        },
+        {
+          url: 'https://github.com/irokaru/pixel-scaler',
+          icon: ['fab', 'github'],
+          text: 'リポジトリ',
+        },
+        {
+          url: 'https://nononotyaya.booth.pm/items/2517679',
+          icon: ['fas', 'images'],
+          text: 'Booth',
+        },
+      ],
+    };
+  },
+}
+</script>
