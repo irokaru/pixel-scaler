49de849fd5917d3e0498296ba37407b42326a365
irokaru <karuta@nononotyaya.net>
2022-01-20 21:03:40 +0900

    add English setting file

--- src/i18n/lang.js
+++ src/i18n/lang.js
@@ -1,12 +1,16 @@
 import { createI18n } from 'vue-i18n';
+
 import ja from './ja.json';
+import en from './en.json';
+
+import System from '../lib/System';
 
 const messages = {
-  ja
+  ja, en
 };
 
 const i18n = createI18n({
-  locale: 'ja',
+  locale: System.language(),
   fallbackLocale: 'ja',
   messages
 });
