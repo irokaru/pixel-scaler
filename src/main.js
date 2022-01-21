72a9cdfcb531278682cb1f04d8c6956543203b23
irokaru <karuta@nononotyaya.net>
2022-01-21 21:52:28 +0900

    add polyfill for window

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
-require('./lib/Polyfill');
-
 import { library }         from '@fortawesome/fontawesome-svg-core';
 import { fas }             from '@fortawesome/free-solid-svg-icons';
 import { far }             from '@fortawesome/free-regular-svg-icons';
