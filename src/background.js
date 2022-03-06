7a8589944c85c13a0e1bd9e099508187430492ce
irokaru <karuta@nononotyaya.net>
2022-03-06 19:57:18 +0900

    correct menubar

--- src/background.js
+++ src/background.js
@@ -24,6 +24,8 @@ async function createWindow() {
     }
   })
 
+  win.setMenuBarVisibility(false)
+
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
     await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
