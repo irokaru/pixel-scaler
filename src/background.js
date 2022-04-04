ca3bb7eedb785903bbd689437a8d644ecd0eaa26
irokaru <karuta@nononotyaya.net>
2022-04-05 00:13:37 +0900

    off devtools when production mode

--- src/background.js
+++ src/background.js
@@ -20,7 +20,8 @@ async function createWindow() {
     webPreferences: {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION
+      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
+      devTools: isDevelopment,
     }
   })
 
