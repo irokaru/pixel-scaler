e2c60a8dc547479a3c569e018134357991ed9966
irokaru <karuta@nononotyaya.net>
2024-07-24 10:41:21 +0900

    add download method

--- src/core/utils/archive.ts
+++ src/core/utils/archive.ts
@@ -16,3 +16,14 @@ export const filesToZip = async (files: File[]): Promise<Blob> => {
       });
   });
 };
+
+export const downloadBlob = (blob: Blob, filename: string): void => {
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+
+  a.href = url;
+  a.download = filename;
+  a.click();
+
+  URL.revokeObjectURL(url);
+};
