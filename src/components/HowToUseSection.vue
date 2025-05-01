03affe1f73332814630a4504a10ba9ad6a1a6b30
irokaru <karuta@nononotyaya.net>
2025-05-01 09:17:43 +0900

    update i18n files

--- src/components/HowToUseSection.vue
+++ src/components/HowToUseSection.vue
@@ -1,4 +1,24 @@
 <template>
-  <!-- TODO: How to use -->
-  <div>dummy</div>
+  <div class="box margin-tb-2 block">
+    <h2>{{ $t("what-is.heading") }}</h2>
+    <p v-for="step in $tm('what-is.steps')" v-html="step"></p>
+
+    <hr />
+
+    <h2>{{ $t("usage.heading") }}</h2>
+    <ol>
+      <li v-for="step in $tm('usage.steps')">
+        {{ step }}
+      </li>
+    </ol>
+
+    <hr />
+
+    <h2>{{ $t("tips.heading") }}</h2>
+    <ul>
+      <li v-for="tip in $tm('tips.steps')">
+        {{ tip }}
+      </li>
+    </ul>
+  </div>
 </template>
