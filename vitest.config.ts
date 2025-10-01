9898c81c5dacfcf58852e7e1019de562f8bd5024
irokaru <karuta@nononotyaya.net>
2025-10-01 09:20:36 +0900

    update tsconfig and fix config for vite and vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,10 +3,11 @@
 import { fileURLToPath } from "node:url";
 
 import vue from "@vitejs/plugin-vue";
+import tsconfigPaths from "vite-tsconfig-paths";
 import { coverageConfigDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), tsconfigPaths()],
   test: {
     globals: true,
     setupFiles: ["tests/unit/vitest.setup.ts"],
