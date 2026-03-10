f399fa08de1daf53e81939cec85ac76758a7a5f7
irokaru <karuta@nononotyaya.net>
2026-03-10 19:00:00 +0900

    test: update e2e tests for GIF support

--- tests/e2e/specs/convert.spec.ts
+++ tests/e2e/specs/convert.spec.ts
@@ -22,6 +22,8 @@ const UploadItemPaths = [
   "2px.png",
   "1px.jpg",
   "2px.jpg",
+  "1px.gif",
+  "2px.gif",
   "1px_alpha.png",
   "2px_alpha.png",
 ].map((fileName) => path.resolve(FixturePath, fileName));
